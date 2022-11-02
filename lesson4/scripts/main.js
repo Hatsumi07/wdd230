0d272a597fe708af8c9ae42b9c134a394d9f0133
Hatsumi07 <crystalhatsumi07@gmail.com>
2022-11-01 23:07:09 -0500

    commit

--- lesson4/scripts/main.js
+++ lesson4/scripts/main.js
@@ -4,15 +4,21 @@ const add = document.getElementById("addChapter");
 
 add.addEventListener('click', addChapter);
 function addChapter() {
-    const newList = document.createElement("li");
-    const newBtn = document.createElement("button");
-    const textNode = document.createTextNode(favChapter.value);
-    const btnTxt = document.createTextNode("❌");
-    newList.appendChild(textNode);
-    chaptersList.appendChild(newList);
-    newBtn.appendChild(btnTxt);
-    newList.appendChild(newBtn);
-    newBtn.addEventListener('click', popChapter);
+    if (favChapter.value.split("").length <= 0) {
+        document.getElementById("inputError").style.display = "block";
+    } else {
+        document.getElementById("inputError").style.display = "none";
+        const newList = document.createElement("li");
+        const newBtn = document.createElement("button");
+        const textNode = document.createTextNode(favChapter.value);
+        const btnTxt = document.createTextNode("❌");
+        newList.appendChild(textNode);
+        chaptersList.appendChild(newList);
+        newBtn.appendChild(btnTxt);
+        newBtn.setAttribute("aria-label", `close ${favChapter.value}`);
+        newList.appendChild(newBtn);
+        newBtn.addEventListener('click', popChapter);
+    }
 };
 function popChapter () {
     const parent = this.parentNode;
