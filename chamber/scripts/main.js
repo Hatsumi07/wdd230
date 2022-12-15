2fe98d3e43716294962b3aef8f6bea4ff431b83b
Hatsumi07 <crystalhatsumi07@gmail.com>
2022-12-14 22:00:09 -0500

    commit contact

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -159,3 +159,11 @@ function liveDate() {
   console.log(date);
   console.log(dateInput);
 }
+
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
